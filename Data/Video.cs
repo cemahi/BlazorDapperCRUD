b677841c7526f37d3537880b7e771d22b05ad5ce
cem ahi <cem.ahi@multinetinventiv.com>
2020-05-20 00:54:19 +0300

    Added VideoService

---
+++ Data/Video.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDapperCRUD.Data
+{
+    public class Video
+    {
+        public int VideoId { get; set; }
+        public string Title { get; set; }
+        public DateTime DatePublished { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Data/VideoService.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace BlazorDapperCRUD.Data
+{
+    public class VideoService : IVideoService
+    {
+        private readonly SqlConnectionConfiguration _configuration;
+
+        public VideoService(SqlConnectionConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<bool> VideoInsert(Video video)
+        {
+            using (var conn = new SqlConnection(_configuration.Value))
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("Title",video.Title,DbType.String);
+                parameters.Add("DatePublished", video.DatePublished, DbType.Date);
+                parameters.Add("IsActive", video.IsActive, DbType.Boolean);
+
+                const string query = @"INSERT INTO Video (Title, DatePublished, IsActive) VALUES (@Title, @DatePublished, @IsActive)";
+                await conn.ExecuteAsync(query, new {video.Title, video.DatePublished, video.IsActive}, commandType: CommandType.Text);
+            }
+
+            return true;
+        }
+    }
+}
